63ae53e6d77c8b5d09e8a9200d1cd7957370c09f
Diego Longarai Furlan <longarai.furlan@gmail.com>
2025-05-28 08:40:42 +0100

    Abiliti characters

--- characters/characters_abilit.py
+++ characters/characters_abilit.py
@@ -8,13 +8,81 @@ class Ryu:
     def atacar(self):
         return self.forca
 
-    
-
 class Ken:
     def __init__(self):
         self.nome = "Ken"
         self.vida = 100
+        self.forca = 14
+        pass
+
+    def atacar(self):
+        return self.forca
+    
+class Guile:
+    def __init__(self):
+        self.nome = "Guile"
+        self.vida = 100
+        self.forca = 9
+        pass
+
+    def atacar(self):
+        return self.forca
+    
+class Chunli:
+    def __init__(self):
+        self.nome = "Chun-Li"
+        self.vida = 100
+        self.forca = 9
+        pass
+
+    def atacar(self):
+        return self.forca
+
+class EHonda:
+    def __init__(self):
+        self.nome = "E.Honda"
+        self.vida = 100
+        self.forca = 8
+        pass
+
+    def atacar(self):
+        return self.forca
+
+class Blanka:
+    def __init__(self):
+        self.nome = "Blanka"
+        self.vida = 100
+        self.forca = 9
+        pass
+
+    def atacar(self):
+        return self.forca
+    
+class Dhalsim:
+    def __init__(self):
+        self.nome = "Dhalsim"
+        self.vida = 100
+        self.forca = 8
+        pass
+
+    def atacar(self):
+        return self.forca
+    
+class MBison:
+    def __init__(self):
+        self.nome = "M.Bison"
+        self.vida = 105
         self.forca = 12
+        pass
+
+    def atacar(self):
+        return self.forca
+
+class Zangief:
+    def __init__(self):
+        self.nome = "Zangief"
+        self.vida = 100
+        self.forca = 8
         pass
 
     def atacar(self):
